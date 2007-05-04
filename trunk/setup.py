d8c2f3c70bc32652667a581f1d5b4c57a0f9ec35
jvloothuis <jvloothuis@dc9af141-952d-0410-b7e9-ad86cb574dc7>
2007-05-04 20:37:12 +0000

    Added basic setuptools support. The game is now installable and will create the proper executable to launch the game.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,25 +1,19 @@
-#from setuptools import setup, find_packages
-from distutils.core import setup
-from glob import glob
-import shutil
-try:
-    import py2exe
-except ImportError:
-    py2exe_available = False
-else:
-    py2exe_available = True
+from setuptools import setup, find_packages
 
 setup(
-    name='666 Luftballons',
-    version='0.2',
-    author="Gin 'n Pythonic (Thijs Jonkman & Jeroen Vloothuis)",
-    author_email='jeroen.vloothuis@xs4all.nl',
-    description='A action based puzzle game',
-    license='GPL',
-    packages=('lib',),
-    windows=[{
-        'script': '666luftballons.py'
-        }]
+    name="666 Luftballons",
+    version="1.0",
+    description="Puzzle/action game in which you try to stomp devils in "
+                "balloons.",
+    author="Thijs Jonkman & Jeroen Vloothuis",
+    packages=find_packages(),
+    install_requires=[
+       'setuptools',
+       'Pygame',
+    ],
+    entry_points={
+        'console_scripts': [
+            '666luftballons = 666luftballons.main:main',
+        ]
+    },
 )
-
-shutil.copytree('data', 'dist/data')
