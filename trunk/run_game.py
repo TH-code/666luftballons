aed68fa462414555e4047ebd0c46c333c25b89cd
jvloothuis <jvloothuis@dc9af141-952d-0410-b7e9-ad86cb574dc7>
2007-05-04 20:29:49 +0000

    Started to move the game to a more setuptools friendly structure

--- trunk/run_game.py
+++ trunk/run_game.py
@@ -3,7 +3,7 @@
 import sys
 import os
 try:
-    libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib'))
+    libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '666luftballons'))
     sys.path.insert(0, libdir)
 except:
     # probably running inside py2exe which doesn't set __file__
